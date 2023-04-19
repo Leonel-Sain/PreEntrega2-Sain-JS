09714245b0fade77836ae28d89d50326b4cfe0ed
Leonel-Sain <leonel.sain3.16@gmail.com>
2023-04-19 23:23:58 +0200

    proyecto finalizado, pre entrega3, falta arreglar barra de busqueda, para que pinte card en dom

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//DECLARACION DE VARIABLES E ID
 const busqueda = document.querySelector("#search")
 const shopBusqueda = document.querySelector("#shopBusqueda")
 const buttonBusqueda = document.querySelector ("#btnSearch")
@@ -11,10 +12,14 @@ let content
 let comprar
 let carritoContent
 let prodEncontrado
+let restar
+let sumar
+let eliminar
+//get item = obtenemos lo que guardamos con el set item
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 // INICIO DE CARRITO DE COMPRAS
-//Array de productos con objetos
+//ARRAY DE PRODUCTOS CON OBJETOS
 const productos=[
     {id: 1, nombre: "Secador de piso", precio: 450,img:"assets/imagenes/detergente-MF.jpg", cantidad: 1, },
     {id: 2, nombre: "Jabon Skip X 5l", precio: 650, img: "assets/imagenes/jabon-liquido-skip-MF.jpg",cantidad: 1,},
@@ -39,23 +44,7 @@ const productos=[
     {id: 21, nombre: "Desodorante de piso Papaya X 5l", precio: 350, img: "assets/imagenes/jabon-liquido-ariel-MF.jpg",cantidad: 1,},
     {id: 22, nombre: "Desodorante de piso Lysoform X 5l", precio:350 , img: "assets/imagenes/jabon-liquido-ariel-MF.jpg",cantidad: 1,},
 ];
-
-/* let prodLimpieza = JSON.parse(localStorage.getItem("productos")) || productos;
- */
-////////////////////////METODO DE FILTRADO NO SOLUCIONADO//////////////////////////////////////////////////////////////
-/* const filtrarProductos = () =>{
-    console.log(busqueda.value);
-    const texto = filtrarProductos.value.toLowerCase();
-    for(prodEncontrado of productos){
-        let nombre = prodEncontrado.nombre.toLowerCase();
-        if(nombre)
-    }
-} 
-buttonBusqueda.addEventListener("click", filtrarProductos)*/
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/* CARD PRODUCT */
-
+//INTENTO DE BARRA DE BUSQUEDA, por consola busca, pero no pinta las card en dom
 const buscador = () => {
     let inputTexto = document.getElementById('search');
     inputTexto.addEventListener('change', () => {
@@ -64,7 +53,6 @@ const buscador = () => {
       prodEncontrado= productos.filter((productos) =>
         productos.nombre.includes(buscador.toUpperCase())
       );
-
       shopBusqueda.innerHTML = '';
 
       prodEncontrado.forEach((productos) => {
@@ -90,13 +78,7 @@ const buscador = () => {
     });
   };
     buscador ();
-
-//Listeners de búsqueda
-/* search.addEventListener("input", () => {
-    let nuevoFiltro = filtrar(productos, search.value, "search");
-    crearHtml(nuevoFiltro);
-  }); */
-  
+//CREACION DE DIV + CARD
 productos.forEach((product)=>{
     content = document.createElement("div");
     content.className = "card";
@@ -116,21 +98,17 @@ productos.forEach((product)=>{
 
         const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
         
-        if(repeat){
-            carrito.map((prod) => {
-                if(prod.id === product.id){
-                    prod.cantidad++;
+(repeat)? carrito.map((prod) => {
+    if(prod.id === product.id){
+            prod.cantidad++;
                 }
-            });
-        }else{
-        carrito.push({
-            id: product.id,
-            img: product.img,
+            })
+: carrito.push({ id: product.id,img: product.img,
             nombre: product.nombre,
             precio: product.precio,
             cantidad: product.cantidad,
         });
-    }
+    
     console.log(carrito);
     carritoCounter();
     saveLocal();
@@ -155,7 +133,7 @@ const pintarCarrito = () => {
         modalContainer.style.display = "none";
     })
     modalHeader.append(modalButton);
-/* carrito de compras */
+/* CARRITO DE COMPRAS */
     carrito.forEach((product) => {
         carritoContent = document.createElement("div")
         carritoContent.className = "modal-content"
@@ -170,7 +148,7 @@ const pintarCarrito = () => {
                 `;
 
         modalContainer.append(carritoContent);
-        let restar = carritoContent.querySelector(".restar")
+        restar = carritoContent.querySelector(".restar")
         restar.addEventListener("click", () =>{
             if(product.cantidad !== 1){
             product.cantidad--;
@@ -179,20 +157,20 @@ const pintarCarrito = () => {
             pintarCarrito();
         });
 
-        let sumar = carritoContent.querySelector (".sumar")
+        sumar = carritoContent.querySelector (".sumar")
         sumar.addEventListener("click", () => {
             product.cantidad++;
             saveLocal();
             pintarCarrito();
         });
 
-        let eliminar = document.createElement("span");
+        eliminar = document.createElement("span");
         eliminar.innerText= "✖";
         eliminar.className= "delete-product";
         carritoContent.append(eliminar)
         eliminar.addEventListener("click", eliminarProductos);
     });
-    /*  precio final de productos  (el total)*/
+/* PRECIO FINAL DE LOS PRODUCTOS (el total)*/
     const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
 
     const totalBuying = document.createElement("div")
@@ -212,228 +190,15 @@ const eliminarProductos = () => {
     saveLocal();
     pintarCarrito();
 };
-//contador
+//CONTADOR
 const carritoCounter = () => {
     cantidadCarrito.style.display = "block";
     const carritoLength = carrito.length;
     localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
     cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
-
 };
 carritoCounter();
-
 //set item = para setiar a localstorage mandar informacion
 const saveLocal = () => {
     localStorage.setItem("carrito", JSON.stringify (carrito));
 };
-//get item = obtenemos lo que guardamos con el set item
-
-
-
-
-
-//Declaracion de variables
-//let nombreUser
-
-
-
-
-
-/* 
-let seleccion 
-let producto 
-let masProducto
-let degrVariedad
-
-let todosLosProductos = productos.map((productos) => productos.nombre + " " + productos.precio + "$" );
-let precio = 0
-let unidades
-let prodEncontrado
-let oferta */
-
-
-//Funciones de búsqueda
-/* function buscarServicio(arr, filtro) {
-    const encontrado = arr.find((el) => {
-      return el.nombre.includes(filtro);
-    });
-    return encontrado;
-  }
-  function filtrarServicio(arr, filtro) {
-    const filtrado = arr.filter((el) => {
-      return el.nombre.includes(filtro);
-    });
-    return filtrado;
-  }
-  const btnSearch = document.querySelector("#btnSearch");
-  const inputs = document.querySelectorAll('input[type="text"]'); */
-//Inicio de ingreso
-/* function welcome(nombre){
-    alert("Bienvenido/a" + " " + nombre);
-}
-nombreUser= prompt("Ingresa tu nombre ");
-   welcome(nombreUser)
-    alert("MF productos de limpieza te da la bienvenida");
-    
-
-//filtro de busqueda por precio (ofertas)
-prodEncontrado = prompt("Ingrese un monto menor o igual a 300 para ver las oferta disponibles \ningrese no para ir al menu principal")
-
-while(prodEncontrado >"300" && prodEncontrado != "no"){
-        alert ("ingrese un monto menor o igual a 300 para ver las oferta disponibles \n ingrese no para ir al menu principal")
-        prodEncontrado = prompt("ingrese un monto menor o igual a 300 para ver las oferta disponibles \n ingrese no para ir al menu principal")
-    }        
-    if (prodEncontrado <="300"){
-        alert("Te presentamos nuestras ofertas!!.")
-        const productOfert= productos.filter(productos => productos.precio <= prodEncontrado);
-        console.log(productOfert)
-        const mapDeOferta = productOfert.map ((prod) => prod.nombre + " " + prod.precio + "$" + " " + prod.oferta )
-        alert(mapDeOferta.join("\n"))
-        }
-//Fin filtro de busqueda
-//Inicio, lista de productos. 
-seleccion= prompt("Hola! desea comprar algun producto de limpieza? ingrese si o no")
-
-while(seleccion!= "si" && seleccion !="no"){
-    alert ("Por favor ingresa si o no")
-    seleccion= prompt("Hola! desea comprar algun producto de limpieza? ingrese si o no")
-    }
-if (seleccion=="si"){
-    alert("Te presentamos nuestra lista de productos.")
-    alert(todosLosProductos.join("\n"))
-    
-    }else if(seleccion == "no"){
-    alert("Gracias por venir, hasta pronto!!")
-    }
-
-//Inicio de productos y while, para evaluar los casos
-while(seleccion!= "no"){
-     producto = prompt(
-        "Seleccione el o los productos que desee añadir a su carritos de compras\n1 Secador de piso \n2 Jabon Skip X 5l \n3 Jabon Ariel X 5l\n4 Suavizante Vivere X 5l \n5 Suavizante Confort X 5l \n6 Suavizante Triple fragancia X 5l \nx Mas productos \n8 Lista de ofertas");    
-    if(producto == "1" || producto == "2" || producto == "3" || producto == "4" || producto == "5" || producto == "6" || producto == "x" || producto == "8"){
-        switch (producto) {
-            case "1":
-                alert(precio = 450)
-                break;
-            case "2":
-                alert(precio = 650)
-                break;
-            case "3":
-                alert(precio = 500)
-                break;
-            case "4":
-                alert(precio = 450)
-                break;
-            case "5":
-                alert(precio = 650)
-                break;
-            case "6":
-                alert(precio = 500)
-                break;
-                    default:
-            case"x":
-            masProducto = prompt(
-                "Mas productos de limpieza \n1 Lavandina X 5l \n2 Detergente X 5l \n3 Desengrasante X 5l\n4 Perfumina X 1/2 L \n5 Desodorantes de piso ");
-                    if(masProducto == "1" || masProducto == "2" || masProducto == "3" ||masProducto == "4" || masProducto == "5" );{
-            switch(masProducto){
-                case "1":
-                    alert(precio = 350)
-                    break;
-                case "2":
-                    alert(precio = 650)
-                    break;
-                case "3":
-                    alert(precio = 400)
-                    break;
-                case "4":
-                    alert(precio = 350)
-                    break;
-                    default:
-                case "5":
-            degrVariedad= prompt(
-                "Seleccione una fragancia de pisos \n1 Citronela X 5l \n2 Espadol X 5l \n3 Procenex X 5l\n4 Bebe X 5l\n5 Lavanda X 5l \n6 Chicle X 5l \n7 Papaya X 5l \n8 Lysoform X 5l ");            
-                if(degrVariedad == "1" || degrVariedad == "2" || degrVariedad == "3" || degrVariedad == "4" || degrVariedad == "5" || degrVariedad == "6" || degrVariedad == "7" || degrVariedad == "8" );{
-            switch(degrVariedad){
-                case "1":
-                    alert(precio = 350)
-                    break;
-                case "2":
-                    alert(precio = 350)
-                    break;
-                case "3":
-                    alert(precio = 350)
-                    break;
-                case "4":
-                    alert(precio = 350)
-                    break;
-                case "5":
-                    alert(precio = 350)
-                    break;
-                case "6":
-                    alert(precio = 350)
-                    break;
-                case "7":
-                    alert(precio = 350)
-                    break;
-                case "8":
-                    alert(precio = 350)
-                    break;
-                    default:
-                            }
-                        }  
-                    }  
-                } 
-            case"8":
-        oferta = prompt("Productos de limpieza en oferta! \n1 Jabon Skip X 1l \n2 Jabon Ariel X 1l \n3 Suavizante Vivere X 1l\n4 Suavizante Confort X 1l \n5 Desodorante de piso Citronela X 5l \n6 Desodorante de piso Procenex X 5l  \n7 Desodorante de piso Chicle X 5l ");
-            if(oferta== "1" || oferta == "2" || oferta == "3" ||oferta == "4" || oferta == "5" || oferta == "6" || oferta == "7" );{
-                switch(oferta){
-                    case "1":
-                        alert(precio = 120)
-                        break;
-                    case "2":
-                        alert(precio = 150)
-                        break;
-                    case "3":
-                        alert(precio = 100)
-                        break;
-                    case "4":
-                        alert(precio = 120)
-                        break;
-                    case "5":
-                        alert(precio = 250)
-                        break;
-                    case "6":
-                        alert(precio = 250)
-                        break;
-                    case "7":
-                        alert(precio = 250)
-                        break;
-                        default:
-                    }
-        }
-    }
-unidades =parseInt(prompt("cuantas unidades desea llevar ?"))
-carrito.push({producto, masProducto, degrVariedad, unidades, precio})
-    }
-    else{alert("no tenemos ese producto")
-} 
-seleccion= prompt("Desea seguir comprando? ingrese: si o no ?")
-//Finalizando compra y devolviendo los precios de los productos.
-while(seleccion === "no"){
-    alert("gracias por su compra, hasta pronto! ")
-    carrito.forEach((carritoFinal)=> {
-    alert(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}
-    ,total a pagar por producto ${ carritoFinal.unidades * carritoFinal.precio} `)
-    })
-break;
-} 
-}
-//Final compra, devolucion total de todos los productos.
-const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
-alert (`El total a pagar por su compra es: ${total}`)
- */
-
-/* const jabonSkip = document.querySelector("#jabonSkip")
-
-jabonSkip.innerText ="Ariel"
-console.log(jabonSkip); */
